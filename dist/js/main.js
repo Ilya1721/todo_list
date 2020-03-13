ba57d7d8cdf922db94cdae46f0f3cf1ffea2851c
unknown <ilyavusnya@gmail.com>
2020-03-13 21:16:29 +0200

    Commit

--- dist/js/main.js
+++ dist/js/main.js
@@ -6,9 +6,14 @@ var checkboxInputs = document.getElementsByClassName('checkbox-input');
 [...checkboxInputs].forEach(c => c.addEventListener('change', checkItem));
 var clearBtn = document.getElementById('clearBtn');
 clearBtn.addEventListener('click', clearItems);
+var saveBtn = document.getElementById('saveBtn');
+saveBtn.addEventListener('click', saveItems);
+window.addEventListener("load", onLoad);
+var localStorage = window.localStorage;
 
 function addItem() {
-  var inputVal = document.getElementById('todo').value;
+  var mainInput = document.getElementById('todo');
+  var inputVal = mainInput.value;
   if(inputVal)
   {
     var checkboxDiv = document.getElementById('checkbox-div');
@@ -30,6 +35,7 @@ function addItem() {
     button.classList.add('deleteBtn');
     button.innerHTML = 'X';
     button.addEventListener('click', deleteItem);
+    mainInput.value = "";
 
     checkboxItem.appendChild(input);
     checkboxItem.appendChild(text);
@@ -69,6 +75,68 @@ function checkItem() {
 }
 
 function clearItems() {
+  if(confirm("Are you sure? All items will be deleted!!"))
+  {
+    var checkboxDiv = document.getElementById('checkbox-div');
+    [...checkboxDiv.children].forEach(c => c.remove());
+    localStorage.clear();
+  }
+}
+
+function onLoad() {
+  var localStorageItems = JSON.parse(localStorage.getItem('todoItems'));
+  var length;
+  if(localStorageItems == null)
+  {
+      length = 0;
+  }
+  else
+  {
+    var length = localStorageItems.length;
+  }
+
   var checkboxDiv = document.getElementById('checkbox-div');
-  [...checkboxDiv.children].forEach(c => c.remove());
+
+  for(var i = 0; i < length; i++) {
+    var checkboxItem = document.createElement('div');
+    checkboxItem.classList.add('checkbox-item');
+
+    var input = document.createElement('input');
+    input.setAttribute('type', 'checkbox');
+    input.setAttribute('name', 'item');
+    input.classList.add('checkbox-input');
+    input.addEventListener('change', checkItem);
+
+    var text = document.createElement('span');
+    text.classList.add('text');
+    if(localStorageItems[i].checked) {
+      text.classList.add('line-through');
+    }
+    text.innerHTML = localStorageItems[i].name;
+    input.checked = localStorageItems[i].checked;
+
+    var button = document.createElement('button');
+    button.setAttribute('type', 'button');
+    button.classList.add('deleteBtn');
+    button.innerHTML = 'X';
+    button.addEventListener('click', deleteItem);
+
+    checkboxItem.appendChild(input);
+    checkboxItem.appendChild(text);
+    checkboxItem.appendChild(button);
+    checkboxDiv.appendChild(checkboxItem);
+  }
+}
+
+function saveItems() {
+  var items = document.getElementsByClassName("checkbox-input");
+  var itemTexts = document.getElementsByClassName("text");
+  var newItems = [];
+  for(var i = 0; i < items.length; i++) {
+    newItems.push({
+      name: itemTexts[i].innerHTML,
+      checked: items[i].checked
+    });
+    localStorage.setItem('todoItems', JSON.stringify(newItems));
+  }
 }
